1db642b468f86452fcd07c08cbee4ff416900d91
Matt Blackman <mateoblackman@gmail.com>
2020-08-31 13:59:33 -0600

    Added a customer Matcher for comparing all items in a list. Added more tests for OrderDetailsDaoTest.kt

--- app/src/androidTest/java/app/mblackman/oderfulfillment/data/database/OrderDetailsDaoTest.kt
+++ app/src/androidTest/java/app/mblackman/oderfulfillment/data/database/OrderDetailsDaoTest.kt
@@ -6,10 +6,10 @@ import androidx.lifecycle.Observer
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import app.mblackman.oderfulfillment.utils.ListMatcherFactory
 import app.mblackman.orderfulfillment.data.database.OrderDetails
 import app.mblackman.orderfulfillment.data.database.OrderDetailsDao
 import app.mblackman.orderfulfillment.data.database.StoreDatabase
-import org.hamcrest.core.IsEqual.equalTo
 import org.junit.After
 import org.junit.Assert.assertThat
 import org.junit.Before
@@ -28,6 +28,13 @@ class OrderDetailsDaoTest {
     private lateinit var orderDetailsDao: OrderDetailsDao
     private lateinit var storeDatabase: StoreDatabase
 
+    private val detailsListMatcherFactory = ListMatcherFactory<OrderDetails> { expected, result ->
+        orderDetailsCompare(
+            expected,
+            result
+        )
+    }
+
     @Before
     fun setUp() {
         observer = Observer<List<OrderDetails>> {}
@@ -45,22 +52,73 @@ class OrderDetailsDaoTest {
     @Test
     @Throws(Exception::class)
     fun writeOrderDetailAndReadList() {
-        val orderDetails = Utils.createOrderDetails(orderDetailsId = 1)
+        val orderDetails = Utils.createOrderDetails()
 
         orderDetailsDao.insertAll(listOf(orderDetails))
         val storedOrderDetails = orderDetailsDao.getOrderDetails()
         storedOrderDetails.observeForever(observer)
 
-        assertThat(
-            "Only a single element was expected",
-            storedOrderDetails.value?.size,
-            equalTo(1)
-        )
+        assertThat(storedOrderDetails.value, detailsListMatcherFactory.create(listOf(orderDetails)))
+    }
 
+    @Test
+    @Throws(Exception::class)
+    fun getByAdapterMatchesAdapterId() {
+        val adapter1OrderDetails = createAdapterUsers(1, 3)
+        val adapter2OrderDetails = createAdapterUsers(2, 3)
+        val adapter3OrderDetails = createAdapterUsers(3, 3)
+        val allItems = adapter1OrderDetails + adapter2OrderDetails + adapter3OrderDetails
+
+        orderDetailsDao.insertAll(allItems)
+        val storedOrderDetailsAdapter1 = orderDetailsDao.getOrderDetailsByAdapter(1)
+        val allStoredOrderDetails = orderDetailsDao.getOrderDetails()
+        allStoredOrderDetails.observeForever(observer)
+
+        assertThat(allStoredOrderDetails.value, detailsListMatcherFactory.create(allItems))
         assertThat(
-            "Order details from database should match properties from inserted item.",
-            storedOrderDetails.value?.get(0),
-            equalTo(orderDetails)
+            storedOrderDetailsAdapter1,
+            detailsListMatcherFactory.create(adapter1OrderDetails)
         )
     }
+
+    /**
+     * Validates the database maintains a single [OrderDetails] with a certain adapter id and
+     * adapter entity id.
+     */
+    @Test()
+    @Throws(Exception::class)
+    fun singleRemoteEntityUpdate() {
+        val adapterId = 1
+        val adapterEntityId = 100L
+        val originalOrderDetails = Utils.createOrderDetails(
+            adapterId = adapterId,
+            adapterEntityKey = adapterEntityId,
+            buyerName = "original"
+        )
+        val replacedOrderDetails = Utils.createOrderDetails(
+            adapterId = adapterId,
+            adapterEntityKey = adapterEntityId,
+            buyerName = "replaced"
+        )
+
+        orderDetailsDao.insertAll(listOf(originalOrderDetails))
+        orderDetailsDao.insertAll(listOf(replacedOrderDetails))
+        val storedOrderDetails = orderDetailsDao.getOrderDetails()
+        storedOrderDetails.observeForever(observer)
+
+        assertThat(
+            storedOrderDetails.value,
+            detailsListMatcherFactory.create(listOf(replacedOrderDetails))
+        )
+    }
+
+    private fun createAdapterUsers(adapterId: Int, numUsers: Int = 3) =
+        (1..numUsers).map {
+            val id = it.toLong()
+            Utils.createOrderDetails(
+                adapterId = adapterId,
+                adapterEntityKey = id,
+                buyerName = "Adapter $adapterId User $it"
+            )
+        }
 }
---
+++ app/src/androidTest/java/app/mblackman/oderfulfillment/utils/Matchers.kt
@@ -0,0 +1,46 @@
+package app.mblackman.oderfulfillment.utils
+
+import org.hamcrest.BaseMatcher
+import org.hamcrest.Description
+
+/**
+ * A [BaseMatcher] that uses a custom equal check for all the items in a list. Expects the items
+ * in the list to be in the same order.
+ *
+ * @param expected The expected collection to compare with.
+ * @param matchCompare The compare function to compare items in the list.
+ */
+class ListMatcher<T>(
+    private val expected: List<T>,
+    private val matchCompare: (T, Any?) -> Boolean
+) : BaseMatcher<List<T>>() {
+
+    override fun describeTo(description: Description?) {
+        description?.appendValue(expected)
+    }
+
+    override fun matches(item: Any?): Boolean {
+        if (item == null || item !is List<*> || item.size != expected.size) {
+            return false
+        }
+
+        for (i in (expected.indices)) {
+            if (!this.matchCompare(expected[i], item[i])) {
+                return false
+            }
+        }
+
+        return true
+    }
+
+}
+
+/**
+ * Used to create a [ListMatcher] with a given compare function.
+ *
+ * @param matchCompare The function to compare items.
+ */
+class ListMatcherFactory<T>(private val matchCompare: (T, Any?) -> Boolean) {
+    fun create(expected: List<T>): ListMatcher<T> =
+        ListMatcher(expected, matchCompare)
+}
